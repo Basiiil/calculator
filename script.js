39fc84aa2eb6beeece4d3537be789b1ad5d65fda
Basiiil <basil.pros23@gmail.com>
2022-10-04 10:59:41 +0330

    fixed calculator them

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == 'clear') {
       display.innerText = '';
-    } else if(item.id == 'backspace'){
+    } else if (item.id == 'backspace') {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText !='' && item.id == 'equal') {
+    } else if (display.innerText != '' && item.id == 'equal') {
       display.innerText = eval(display.innerText);
     } else if (display.innerText == '' && item.id == 'equal') {
       display.innerText = 'Empty!';
@@ -17,4 +17,16 @@ buttons.forEach((item) => {
       display.innerText += item.id;
     }
   }
-})
+});
+
+// Them
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
+
